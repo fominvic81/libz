f44f3e6baafc184a9e11865d21ff3daf25e0785c
Andrew Kelley <andrew@ziglang.org>
2023-10-10 17:44:54 -0700

    update for zig 0.12.0-dev.878+7abf9b3a8

--- build.zig
+++ build.zig
@@ -7,23 +7,26 @@ pub fn build(b: *std.build.Builder) void {
         .optimize = b.standardOptimizeOption(.{}),
     });
     lib.linkLibC();
-    lib.addCSourceFiles(&.{
-        "adler32.c",
-        "crc32.c",
-        "deflate.c",
-        "infback.c",
-        "inffast.c",
-        "inflate.c",
-        "inftrees.c",
-        "trees.c",
-        "zutil.c",
-        "compress.c",
-        "uncompr.c",
-        "gzclose.c",
-        "gzlib.c",
-        "gzread.c",
-        "gzwrite.c",
-    }, &.{"-std=c89"});
+    lib.addCSourceFiles(.{
+        .files = &.{
+            "adler32.c",
+            "crc32.c",
+            "deflate.c",
+            "infback.c",
+            "inffast.c",
+            "inflate.c",
+            "inftrees.c",
+            "trees.c",
+            "zutil.c",
+            "compress.c",
+            "uncompr.c",
+            "gzclose.c",
+            "gzlib.c",
+            "gzread.c",
+            "gzwrite.c",
+        },
+        .flags = &.{"-std=c89"},
+    });
     lib.installHeader("zconf.h", "zconf.h");
     lib.installHeader("zlib.h", "zlib.h");
     b.installArtifact(lib);
